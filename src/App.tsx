d3058f09dda9f26a91b80303792b87265f0818a2
Chanaphat <chanaphat@iapp.co.th>
2023-03-13 19:33:03 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Home from '@pages/Home/Home'
+function App() {
+
+  return (
+    <div className="App">
+      <Home/>
+      asfas
+    </div>
+  )
+}
+
+export default App
