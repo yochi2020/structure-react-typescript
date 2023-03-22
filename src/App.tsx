c90273188510f550f71cbcce486c1b16e457f72e
Chanaphat Suzuki <chanaphatsuzuki@Chanaphats-MacBook-Air.local>
2023-03-22 14:02:51 +0700

    setup redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import Home from '@pages/Home/Home'
 function App() {
 
   return (
     <div className="App">
       <Home/>
-      asfas
     </div>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { RootState } from '@/store/store'
+import {useDispatch,useSelector} from 'react-redux'
+import {decrement,increment} from '../../store/slices/couterSlice'
 type Props = {}
 
 const Home = (props: Props) => {
+  const count = useSelector((state:RootState)=>state.counter.value)
+  const dispatch = useDispatch()
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <br />
+      {count}
+      <button onClick={()=>dispatch(decrement())}>-</button>
+      <button onClick={()=>dispatch(increment())}>+</button>
+    </div>
   )
 }
 
